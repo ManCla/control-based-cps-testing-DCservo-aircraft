eec8d9e5db0034f2c123b687e7468277fd2ecbb8
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-08-21 14:48:14 +0200

    implemented test case generation based on shape amplitude and time scaling

--- run_once.m
+++ run_once.m
@@ -1,9 +1,9 @@
 
-% path containig the simulink model
-addpath('model/');
+addpath('model/')   % path containig the simulink model
+addpath('testing')  % path for implementation of testing approach
 
 %% variables for readable naming
-% NOTE: the values are actually important as they are used for routing by
+% NOTE: the actual values are important as they are used for routing by
 %       the Multi-Port Switch simulink block.
 inl_none      = 0; % no input non-linearity
 inl_dead_zone = 1; % dead-zone input non-linearity
@@ -15,6 +15,10 @@ fnl_coulomb   = 2; % coulomb friction (static+linear)
 
 %% test parameters
 
+num_periods = 5;      % number of input periods included in test
+sampling_time = 0.05; % should be taken from SUT
+settle_time = 5;      % extra time in test for allowing for transients
+
 input_non_linearity = inl_none;
 friction_non_linearity = fnl_linear;
 
@@ -23,13 +27,18 @@ if input_non_linearity~=0 && friction_non_linearity~=0
     disp('Are you sure you want to apply two non-linearities at once?')
 end
 
-test_duration = 30;   % how long the test will last
-reference = [0,  0;   % test input
-             1, 50;
-             2, 0];
+% reference definition
+shape = 'steps';
+amplitude = 5;
+time_scaling = 0.1;
+[reference, test_duration] = test_case(shape, amplitude, time_scaling,...
+                                       num_periods, sampling_time, settle_time);
 
 %% test execution
 
 sim_output = sim('DCservo.slx');
-test_results = sim_output.data;
+test_results = sim_output.data; % output traces extraction
+
+% plot results
+figure(2)
 plot(test_results)
---
+++ testing/test_case.m
@@ -0,0 +1,101 @@
+%{
+Function that computres the test-case duration and reference matrix for a
+given test case.
+A test case is defined by:
+    - shape
+    - amplitude
+    - time_scale
+OUTPUT:
+    - a 2 column matrix [times, values]
+    - duration of test (should be equal to times(end)+sampe_time)
+
+To add a shape:
+    - add it to list in get_shapes_list function
+    - implement function
+    - add shape case to swith statement
+Shape functions shall be defined over a unit period and have have an
+amplitude range of 1 (i.e. max(shape)-min(shape)==1 ).
+They also generate a signal centered around 0.
+%}
+
+function [reference, test_duration] = test_case(shape, amplitude, time_scale, num_periods, sample_time, settle)
+
+    period_duration = 1 / time_scale;
+    test_duration   = period_duration * num_periods + settle;
+    num_samples     = floor(test_duration/sample_time) + 1;
+    time_vector     = linspace(0,test_duration,num_samples);
+
+    switch shape
+        case 'sinus'
+            ref = amplitude.*arrayfun(@sinus, time_scale*time_vector);
+        case 'steps'
+            ref = amplitude.*arrayfun(@steps, time_scale*time_vector);
+        case 'ramp'
+            ref = amplitude.*arrayfun(@ramp, time_scale*time_vector);
+        case 'trapezoidal'
+            ref = amplitude.*arrayfun(@trapezoidal, time_scale*time_vector);
+        case 'triangular'
+            ref = amplitude.*arrayfun(@triangular, time_scale*time_vector);
+        otherwise
+            disp('ERROR: input shape not found')
+    end
+
+    reference = [time_vector', ref'];
+
+end
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%
+%%% SHAPE FUNCTIONS %%%
+%%%%%%%%%%%%%%%%%%%%%%%
+
+function shapes = get_shapes_list()
+    shapes = ['steps', 'ramp', 'trapezoidal', 'triangular', 'sinus'];
+end
+
+function val = sinus(t)
+    val = sin(t*(2*pi));
+end
+
+function val = steps(t)
+    period_percentage = mod(t,1);
+    if period_percentage<0.5
+        val = 0.5;
+    else
+        val = -0.5;
+    end
+end
+
+function val = ramp(t)
+    val= mod(t+0.5,1)-0.5;
+end
+
+function val = trapezoidal(t)
+    t = t+0.125; % time shift to avoid jump at start
+    period_percentage = mod(t,1);
+    % NOTE trapezoidal defined over 0-1 then shifted down by 0.5
+    if period_percentage<0.25         % ramp up
+        val = (period_percentage)/0.25;
+    elseif period_percentage<0.5      % constant high
+        val = 1;
+    elseif period_percentage<0.75     % ramp down
+        val = 1-(period_percentage-0.5)/0.25;
+    else
+        val = 0;
+    end
+    val = val-0.5;
+end
+
+function val = triangular(t)
+    t = t+0.25; % time shift to avoid jump at start
+    period_percentage = mod(t,1);
+    % NOTE trapezoidal defined over 0-1 then shifted down by 0.5
+    if period_percentage<0.5         % ramp up
+        val = (period_percentage)/0.5;
+    else
+        val = 1-(period_percentage-0.5)/0.5;
+    end
+    val = val-0.5;
+end
+
