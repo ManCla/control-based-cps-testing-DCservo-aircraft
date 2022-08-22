d916fe7b1d460c1d7318dec3d20ff0675abef8fb
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-08-22 12:31:43 +0200

    improved plotting and include zero frequency

--- plot_single_test.m
+++ plot_single_test.m
@@ -6,6 +6,9 @@ shape = 'steps';
 amplitude = 5;
 time_scaling = 0.1;
 
+settle_time = 5;
+sampling_time = 0.05;
+
 file_path = sprintf('%s%s-%g-%g.csv',data_directory,shape,amplitude,time_scaling);
 test_results = readmatrix(file_path);
 
@@ -13,19 +16,23 @@ test_results = readmatrix(file_path);
 time = test_results(:,1);
 data = test_results(:,2:end);
 
-%% analyse and plot results
+%% analyse
 % full spectra of reference and output
 [ref_freqs,ref_amps] = fourier_transform_wrap(test_results(settle_time/sampling_time:end,2), sampling_time);
 [out_freqs,out_amps] = fourier_transform_wrap(test_results(settle_time/sampling_time:end,3), sampling_time);
 % main components
 [ref_freq_peaks,ref_amp_peaks] = fA_main_components(test_results(:,2), sampling_time, settle_time);
-[out_freq_peaks,put_amp_peaks] = fA_main_components(test_results(:,3), sampling_time, settle_time);
-% degree of non-linearity
-
-% filtering degree
+[out_freq_peaks,out_amp_peaks] = fA_main_components(test_results(:,3), sampling_time, settle_time);
+% compute degree of non-linearity
+dnl = non_linearity_degree(test_results(:,3),sampling_time,settle_time,ref_freq_peaks,ref_amp_peaks);
+disp("Non-Linearity degree is: ")
+disp(dnl)
+% compute filtering degree
 dof = filtering_degree(test_results(:,3),sampling_time,settle_time,ref_freq_peaks,ref_amp_peaks);
 disp("Filtering degree is: ")
 disp(dof)
+
+%% plot
 
 figure(2)
 plot(time,data),grid
@@ -33,8 +40,18 @@ plot(time,data),grid
 figure(3)
 clf
 hold on
+% just for plotting purposes otherwise zero frequency does not show up in
+% logarithmic scale
+ref_freqs(1) = 0.001;
+out_freqs(1) = 0.001;
+if ref_freq_peaks(1)==0
+    ref_freq_peaks(1) = 0.001;
+    out_freq_peaks(1) = 0.001;
+end
+% scatter plot full spectra
 scatter(ref_freqs,ref_amps,5,'blue','o')
 scatter(out_freqs,out_amps,5,'red','o')
+% scatter plot main components
 scatter(ref_freq_peaks,ref_amp_peaks,80,'blue','x')
 scatter(out_freq_peaks,out_amp_peaks,80,'red','x')
 grid
