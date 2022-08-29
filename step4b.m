b6dbc20bf48371f7f88392f629875c1e1ccd2705
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-08-29 17:15:53 +0200

    reorganized plots

--- step4b.m
+++ step4b.m
@@ -1,8 +1,8 @@
 %{
 Implement plotting part of step 4 of apporach.
 We display:
- (Fig.1)   fA plot with non-linearity and filtering degree by shapes
- (Fig.2)   fA plot with non-linearity and filtering degree
+ (Fig.1)   fA plot with non-linearity by shapes and all shapes together
+ (Fig.2)   fA plot with filtering degree all shapes together
  (Fig.3)   plot of degree of filtering over the frequencies
  - fA plot with ground truth of non-linear phenomena
  
@@ -15,9 +15,15 @@ dot_size = 8; % size of dots in all scatter plot
 f_min_plot = f_min;
 f_max_plot = 10;
 
+%% open ile containing sinusoidal based upperbound of nonlinear threshold
+% this is used for  plotting the nlth as reference
+nlth_file_path = sprintf("%s/nlth_upper_bound_fmin%g_fmax%g_damp%g_amax%g.csv", ...
+                         directory,f_min,f_max,delta_amp,amplitude_max);
+nlth_upper_bound = readmatrix(nlth_file_path);
+
 %% plot dnl and dof by shapes
 
-fa_all = [];
+fa_all = []; % init variable to collect fA points for all shapes
 
 % iterate over shapes
 for s_idx = 1:length(shapes)
@@ -34,19 +40,17 @@ for s_idx = 1:length(shapes)
     % figure by shapes
     figure(1)
     subplot(length(shapes),2,s_idx*2-1)
+    hold on
     scatter(fa_points(:,1),fa_points(:,2),dot_size,'filled','CData',dnl)
+    plot(nlth_upper_bound(:,1), nlth_upper_bound(:,2))
+    plot(nlth_upper_bound(:,1), nlth_upper_bound(:,3))
+    hold off
     colorbar
-    ylabel(shapes(s_idx))
+    title(shapes(s_idx))
+    ylabel('Amplitude')
     xlim([f_min_plot, f_max_plot])
     ylim([0 7])
-    grid on
-    set(gca,'xscale','log')
-    set(gca,'yscale','log')
-    subplot(length(shapes),2,s_idx*2)
-    scatter(fa_points(lin_indexes,1),fa_points(lin_indexes,2),dot_size,'filled','CData',dof(lin_indexes))
-    colorbar
-    ylim([0 7])
-    xlim([f_min_plot, f_max_plot])
+    clim([0,nl_threshold])
     grid on
     set(gca,'xscale','log')
     set(gca,'yscale','log')
@@ -63,16 +67,32 @@ for s_idx = 1:length(shapes)
     
 end % iteration over shapes
 
-% add titles and labels
+%% plot dnl and dof all shapes together
+lin_indexes_all = fa_all(:,3)<nl_threshold; % filter out points that show non linear behaviour
+dnl_all = nl_threshold-min(fa_all(:,3),nl_threshold);
+dof_all = min(fa_all(:,4),1);
+
+% dnl plot for all shapes on right-hand side of figure
 figure(1)
-subplot(length(shapes),2,1)
-title('Degre of Non-Linearity')
-subplot(length(shapes),2,2)
-title('Degre of Filtering')
+sgtitle('Degree of Non-Linearity')
 subplot(length(shapes),2,2*length(shapes)-1)
 xlabel('Frequency')
-subplot(length(shapes),2,2*length(shapes))
+subplot(1,2,2)
+hold on
+scatter(fa_all(:,1),fa_all(:,2),dot_size,'filled','CData',dnl_all)
+plot(nlth_upper_bound(:,1), nlth_upper_bound(:,2))
+plot(nlth_upper_bound(:,1), nlth_upper_bound(:,3))
+hold off
+colorbar
+title('Degree of Non-Linearity: all shapes together')
 xlabel('Frequency')
+ylabel('Amplitude')
+xlim([f_min_plot, f_max_plot])
+ylim([0 7])
+clim([0,nl_threshold])
+grid on
+set(gca,'xscale','log')
+set(gca,'yscale','log')
 colormap('jet')
 
 % add titles and labels
@@ -81,28 +101,15 @@ xlabel('Frequency')
 subplot(length(shapes),1,1)
 title('Degree of filtering over frequencies')
 
-%% plot dnl and dof all shapes together
-lin_indexes = fa_all(:,3)<nl_threshold; % filter out points that show non linear behaviour
-dnl = min(fa_all(:,3),nl_threshold);
-dof = min(fa_all(:,4),1);
-
 figure(2)
-subplot(1,2,1)
-scatter(fa_all(:,1),fa_all(:,2),dot_size,'filled','CData',dnl)
+hold on
+scatter(fa_all(lin_indexes_all,1),fa_all(lin_indexes_all,2),dot_size,'filled','CData',dof_all(lin_indexes_all))
+plot(nlth_upper_bound(:,1), nlth_upper_bound(:,2))
+plot(nlth_upper_bound(:,1), nlth_upper_bound(:,3))
+hold off
 colorbar
 xlabel('Frequency')
-ylabel('Amplitude')
-title('Degre of Non-Linearity')
-xlim([f_min_plot, f_max_plot])
-ylim([0 7])
-grid on
-set(gca,'xscale','log')
-set(gca,'yscale','log')
-subplot(1,2,2)
-scatter(fa_all(lin_indexes,1),fa_all(lin_indexes,2),dot_size,'filled','CData',dof(lin_indexes))
-colorbar
-xlabel('Frequency')
-title('Degre of Filtering')
+title('Degree of Filtering: all shapes together')
 xlim([f_min_plot, f_max_plot])
 ylim([0 7])
 grid on
