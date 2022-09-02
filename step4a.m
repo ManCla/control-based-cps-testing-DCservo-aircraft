d4dc25c04e06f1cff63aa077297092d7ac5cf497
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-09-02 20:58:18 +0200

    average the abs nonlinear behavoiur ground truth you dumb ass

--- step4a.m
+++ step4a.m
@@ -59,8 +59,8 @@ for s_idx = 1:length(shapes)
         sat_sensor_perc    = saturation_percentage(test_results(:,3));
         % NOTE: we are skipping index 5 because the simulink exit at that
         % index is empty because of a refuso
-        input_nl_avg    = mean(test_results(:,6));
-        friction_nl_avg = mean(test_results(:,7));
+        input_nl_avg    = mean(abs(test_results(:,6)));
+        friction_nl_avg = mean(abs(test_results(:,7)));
         %%%% end analysis part %%%%
 
         % store fA points
